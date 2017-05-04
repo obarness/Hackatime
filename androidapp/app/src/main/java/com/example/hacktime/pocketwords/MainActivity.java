d3283cff4834ba5b2f1e3efd87c2db9272c03ac8
David <david.lichterov@algo.team>
2017-05-04 23:14:19 +0300

    added jsonStringToArrays - prase json string to json object and initiate the view with the data

--- androidapp/app/src/main/java/com/example/hacktime/pocketwords/MainActivity.java
+++ androidapp/app/src/main/java/com/example/hacktime/pocketwords/MainActivity.java
@@ -12,56 +12,57 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String words[] = {
-            "כסא",
-            "חתול",
-            "בית",
-            "כסא",
-            "חתול",
-            "בית",
-            "כסא",
-            "חתול",
-            "בית",
-            "כסא",
-            "חתול",
-            "בית",
-    };
+    private final String jsonTestString = "[{" +
+            "\t\"text\": \"kitchen\"," +
+            "\t\"imageSource\": \"http://travisperkins.scene7.com/is/image/travisperkins/ready-to-fit-kitchen-Orlando-White-IMG-Main?wid=710\"," +
+            "\t\"wordId\": 1," +
+            "\t\"children\": \",2,4\"," +
+            "\t\"isRoot\": \"true\"" +
+            "}, {" +
+            "\t\"text\": \"garden\"," +
+            "\t\"imageSource\": \"http://theunboundedspirit.com/wp-content/uploads/2013/11/flower-garden.jpg\"," +
+            "\t\"wordId\": 3," +
+            "\t\"children\": \"\"," +
+            "\t\"isRoot\": \"true\"" +
+            "}]";
+    //private String words[] = {};
+    //List<String> words = new ArrayList<>();
+     HashMap<String, String> words = new HashMap<>();
 
-    private final String words_image_urls[] = {
-            "http://www.asha-gabay.co.il/_Uploads/dbsArticles/11500(2).jpg",
-            "http://www.best-friends.co.il/f-users/user_105106/website_105116/images/thumbs/W_960_6824-25.jpg",
-            "http://www.psagotmerkaz.org/wp-content/uploads/2015/01/p17hjafm6p1r1c1pt3d4rr6ddmp1.jpg",
-            "http://www.asha-gabay.co.il/_Uploads/dbsArticles/11500(2).jpg",
-            "http://www.best-friends.co.il/f-users/user_105106/website_105116/images/thumbs/W_960_6824-25.jpg",
-            "http://www.psagotmerkaz.org/wp-content/uploads/2015/01/p17hjafm6p1r1c1pt3d4rr6ddmp1.jpg",
-            "http://www.asha-gabay.co.il/_Uploads/dbsArticles/11500(2).jpg",
-            "http://www.best-friends.co.il/f-users/user_105106/website_105116/images/thumbs/W_960_6824-25.jpg",
-            "http://www.psagotmerkaz.org/wp-content/uploads/2015/01/p17hjafm6p1r1c1pt3d4rr6ddmp1.jpg",
-            "http://www.asha-gabay.co.il/_Uploads/dbsArticles/11500(2).jpg",
-            "http://www.best-friends.co.il/f-users/user_105106/website_105116/images/thumbs/W_960_6824-25.jpg",
-            "http://www.psagotmerkaz.org/wp-content/uploads/2015/01/p17hjafm6p1r1c1pt3d4rr6ddmp1.jpg",
-    };
+
+    //private String words_image_urls[] = {};
+    //List<String> words_image_urls = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jsonStringToArrays();
         initViews();
 
         // example of how to pull data from server
         // result will hold a string of the json data
-        getHttpQuery("http://172.22.6.60:5000/test",new VolleyCallback(){
-           @Override
-            public void onSuccess(String result){
-                System.out.println(result);
-            }
-        });
+       // getHttpQuery("http://172.22.6.60:5000/test",new VolleyCallback(){
+       //    @Override
+        //    public void onSuccess(String result){
+         //       System.out.println(result);
+          //  }
+        //});
     }
     private void initViews(){
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
@@ -75,12 +76,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private ArrayList<WordCard> prepareData(){
-
         ArrayList<WordCard> word_cards = new ArrayList<>();
-        for(int i=0;i<words.length;i++){
+        Set set = words.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry)iterator.next();
             WordCard wordcard = new WordCard();
-            wordcard.setWord_text(words[i]);
-            wordcard.setWord_image_url(words_image_urls[i]);
+            wordcard.setWord_text(mentry.getKey().toString());
+            wordcard.setWord_image_url(mentry.getValue().toString());
             word_cards.add(wordcard);
         }
         return word_cards;
@@ -96,7 +99,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         callback.onSuccess(response.toString());
-
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -111,4 +113,15 @@ public class MainActivity extends AppCompatActivity {
         void onSuccess(String result);
     }
 
+    private void jsonStringToArrays(){
+        try {
+            JSONArray jsonArr = new JSONArray(jsonTestString);
+            for(int i=0;i<jsonArr.length();i++){
+                JSONObject e = jsonArr.getJSONObject(i);
+                words.put(e.getString("text"),e.getString("imageSource"));
+            }
+        } catch (final JSONException e) {
+            System.out.println("Json parsing error: " + e.getMessage());
+        }
+    }
 }
